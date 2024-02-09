88bfff793ab9c5d7b40fa9d593a00b12a2d80e75
Farcaș Alexandru Alin <alexfarcas2@gmail.com>
2024-02-09 22:38:29 +0200

    Add files via upload

---
+++ I - Introduction/2/2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int>();
+        int number;
+
+        while (true)
+        {
+            Console.WriteLine("Enter a number (enter a number ending in 00 to stop):");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number % 100 == 0 && number != 0)
+                break;
+
+            numbers.Add(number);
+        }
+
+        Console.WriteLine("Entered numbers are:");
+        foreach (int n in numbers)
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
