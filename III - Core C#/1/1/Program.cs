080e5fb7d953a371ada00d620785b00dd4b4d882
Farcaș Alexandru Alin <alexfarcas2@gmail.com>
2024-02-09 22:41:56 +0200

    Add files via upload

---
+++ III - Core C#/1/1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a string:");
+        string inputString = Console.ReadLine();
+
+        int alphabetCount = 0, digitCount = 0, specialCharCount = 0;
+
+        foreach (char c in inputString)
+        {
+            if (char.IsLetter(c))
+                alphabetCount++;
+            else if (char.IsDigit(c))
+                digitCount++;
+            else
+                specialCharCount++;
+        }
+
+        Console.WriteLine($"Number of Alphabets in the string is: {alphabetCount}");
+        Console.WriteLine($"Number of Digits in the string is: {digitCount}");
+        Console.WriteLine($"Number of Special characters in the string is: {specialCharCount}");
+    }
+}
