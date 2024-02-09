8fbb8db0ec33216350006a64b7d2dcdf304e457d
Farcaș Alexandru Alin <alexfarcas2@gmail.com>
2024-02-09 22:47:02 +0200

    Add files via upload

---
+++ III - Core C#/5/5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter a positive integer:");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        bool containsThree = IfNumberContains3(number);
+
+        Console.WriteLine($"Does the number contain digit 3? {containsThree}");
+    }
+
+    static bool IfNumberContains3(int number)
+    {
+        while (number > 0)
+        {
+            if (number % 10 == 3)
+                return true;
+
+            number /= 10;
+        }
+
+        return false;
+    }
+}
---
+++ III - Core C#/6/6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the base number:");
+        int baseNumber = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the exponent:");
+        int exponent = Convert.ToInt32(Console.ReadLine());
+
+        long result = ToThePowerOf(baseNumber, exponent);
+
+        Console.WriteLine($"Result: {baseNumber} raised to the power of {exponent} is {result}");
+    }
+
+    static long ToThePowerOf(int baseNum, int exp)
+    {
+        long result = 1;
+        for (int i = 1; i <= exp; i++)
+        {
+            result *= baseNum;
+        }
+        return result;
+    }
+}
